70cf130a53a8dcbefaf0250f04dc6c8e67a2e2d7
koolerjaebee <koolerjaebee@gmail.com>
2024-04-26 08:29:33 +0000

    feat: add logging & change pdf filename rule

--- JAMA.py
+++ JAMA.py
@@ -2,6 +2,10 @@ import time
 import random
 import requests
 from bs4 import BeautifulSoup
+import logging
+
+
+logging.basicConfig(format='%(levelname)s | %(asctime)s | %(message)s', datefmt='%Y/%m/%d %I:%M:%S %p', filename='logs/JAMA.log', level=logging.INFO)
 
 
 headers = {
@@ -9,45 +13,58 @@ headers = {
 }
 
 flag = True
+page = 1
+
 while flag:
+    try:
+        print('='*120)
+        print(f'page: {page}')
+        logging.info(f'page: {page}')
+        print('='*120)
+        base_url = 'https://jamanetwork.com'
+        url = base_url + f'/searchresults?q=clinical&f_ArticleTypeDisplayName=Research&exPrm_qqq=%7bDEFAULT_BOOST_FUNCTION%7d%22clinical%22&exPrm_hl.q=clinical&page={page}&f_OpenAccessFilter=true'
+        response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
-    page = 1
-    base_url = 'https://jamanetwork.com'
-    url = base_url + f'/searchresults?q=clinical&f_ArticleTypeDisplayName=Research&exPrm_qqq=%7bDEFAULT_BOOST_FUNCTION%7d%22clinical%22&exPrm_hl.q=clinical&page={page}&f_OpenAccessFilter=true'
-    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-    if response.status_code != 200:
-        print('Error:', response.status_code)
-        break
-
-    # # Example of page
-    # html_example = soup.prettify("utf-8")
-    # with open("example.html", "wb") as file:
-    #     file.write(html_example)
-
-    title_h3_tags = soup.find_all('h3', {'class': 'article--title at-sr-item-title-link'})
-    link_a_tags = soup.find_all('a', {'class': 'al-link pdf pdfaccess'})
-
-    pdfs = []
-    for i in range(len(title_h3_tags)):
-        pdfs.append({'title': title_h3_tags[i].text.strip().replace('\n', ' '), 'link': base_url + link_a_tags[i].get('data-article-url')})
-    for pdf in pdfs:
-        response = requests.get(pdf['link'], headers=headers, stream=True, allow_redirects=True)
-        if(response.status_code != 200):
+        soup = BeautifulSoup(response.text, 'html.parser')
+        if response.status_code != 200:
             print('Error:', response.status_code)
-        else:
-            with open(f'./data/{pdf["title"]}.pdf', 'wb') as file:
-                print(f'Downloading {pdf["title"]}')
-                file.write(response.content)
-            # with open(f'./data/{pdf["title"]}.txt', 'a') as file:
-            #     file.write(str(response.text))
-            print(type(response.content))
-            print(str(response.content)[:1000])
-            print('='*150)
-            print(str(response.content)[-1000:])
-            print(f'{pdf["title"]} downloaded\n')
+            logging.error(f'Error: {response.status_code}')
+            flag = False
+            break
 
-            time.sleep(random.randint(1, 3))
+        # # Example of page
+        # html_example = soup.prettify("utf-8")
+        # with open("example.html", "wb") as file:
+        #     file.write(html_example)
 
+        # title_h3_tags = soup.find_all('h3', {'class': 'article--title at-sr-item-title-link'})
+        link_a_tags = soup.find_all('a', {'class': 'al-link pdf pdfaccess'})
+
+        links = []
+        for i in range(19):
+            links.append(base_url + link_a_tags[i].get('data-article-url'))
+        for link in links:
+            response = requests.get(link, headers=headers, stream=True, allow_redirects=True)
+            if(response.status_code != 200):
+                print('Error:', response.status_code)
+                logging.error(f'Error: {response.status_code}')
+            else:
+                title = link.split('/')[-1].split('?')[0]
+                print('='*120)
+                with open(f'./data/{title}', 'wb') as file:
+                    print(f'Downloading: {title}')
+                    logging.info(f'Downloading: {title}')
+                    file.write(response.content)
+                print('-'*120)
+                print(f'Download Complete: {title}')
+                logging.info(f'Download Complete: {title}')
+                print('='*120)
+
+                time.sleep(random.randint(1, 10)/10)
+    except Exception as e:
+        print('Error:', e)
+        logging.error(f'Error: {e}')
+        flag = False
+        break
+    
     page += 1
