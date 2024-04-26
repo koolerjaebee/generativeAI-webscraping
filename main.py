7cc3d59ad4fe78d166fd0b26fc3ae56460ea8765
koolerjaebee <koolerjaebee@gmail.com>
2024-04-26 09:36:02 +0000

    refactor: generalize using .env

--- main.py
+++ main.py
@@ -5,25 +5,33 @@ import requests
 from bs4 import BeautifulSoup
 import logging
 import re
+from dotenv import load_dotenv
+
+
+load_dotenv()
+SCRAPPING_TITLE = os.getenv("SCRAPPING_TITLE")
+SCRAPPING_BASE_URL = os.getenv("SCRAPPING_BASE_URL")
+STARTING_PAGE = os.getenv("STARTING_PAGE")
+A_TAG_CLASS = os.getenv("A_TAG_CLASS")
+DOWNLOAD_URL = os.getenv("DOWNLOAD_URL")
 
 
 start_time = time.strftime('%Y%m%d_%H%M')
-
-logging.basicConfig(format='%(levelname)s | %(asctime)s | %(message)s', datefmt='%Y/%m/%d %I:%M:%S %p', filename=f'logs/JAMA_{start_time}.log', level=logging.INFO)
+logging.basicConfig(format='%(levelname)s | %(asctime)s | %(message)s', datefmt='%Y/%m/%d %I:%M:%S %p', filename=f'logs/{SCRAPPING_TITLE}_{start_time}.log', level=logging.INFO)
 
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36'
 }
-
 flag = True
-page = 1
+page = STARTING_PAGE
+
 
 while flag:
     try:
         print(f'page: {page}')
         logging.info(f'page: {page}')
-        base_url = 'https://jamanetwork.com'
+        base_url = SCRAPPING_BASE_URL
         url = base_url + f'/searchresults?q=clinical&f_ArticleTypeDisplayName=Research&exPrm_qqq=%7bDEFAULT_BOOST_FUNCTION%7d%22clinical%22&exPrm_hl.q=clinical&page={page}&f_OpenAccessFilter=true'
         response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
@@ -34,18 +42,11 @@ while flag:
             flag = False
             break
 
-        # # Example of page
-        # html_example = soup.prettify("utf-8")
-        # with open("example.html", "wb") as file:
-        #     file.write(html_example)
-
-        # title_h3_tags = soup.find_all('h3', {'class': 'article--title at-sr-item-title-link'})
-        link_a_tags = soup.find_all('a', {'class': re.compile(r'al-link pdf pdfaccess.*')})
-        print(len(link_a_tags))
+        link_a_tags = soup.find_all('a', {'class': re.compile(fr'{A_TAG_CLASS}.*')})
 
         links = []
         for i in range(19):
-            links.append(base_url + link_a_tags[i].get('data-article-url'))
+            links.append(base_url + link_a_tags[i].get(DOWNLOAD_URL))
         for link in links:
             response = requests.get(link, headers=headers, stream=True, allow_redirects=True)
             if(response.status_code != 200):
